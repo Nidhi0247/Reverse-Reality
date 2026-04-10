209b4535ed48e28f4b25e389f58632a9b6cd54dc
Aryesh02 <aryeshsrivastava@gmail.com>
2026-04-10 23:59:33 +0530

    Changes to Page.js - Areana

--- proxy.js
+++ proxy.js
@@ -1,11 +1,17 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth({
+  pages: {
+    signIn: '/login'
+  }
+})
 
 export const config = {
   matcher: [
-    '/arena/:path*',
-    '/api/submit/:path*',
-    '/api/progress/:path*',
-    '/api/level/:path*',
-    '/api/chat/:path*'
-  ]
+    "/arena/:path*",
+    "/api/submit/:path*",
+    "/api/progress/:path*",
+    "/api/level/:path*",
+    "/api/chat/:path*",
+  ],
 }
